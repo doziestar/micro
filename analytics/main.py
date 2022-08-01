159e02e1579feaf3389512c3a5664d16b08db896
doziestar <chidosiky2015@gmail.com>
2022-08-01 07:28:45 +0100

    producer and consumer in python

--- analytics/main.py
+++ analytics/main.py
@@ -1,3 +1,5 @@
+import json
+
 import typer
 from kafka import KafkaConsumer
 
@@ -5,7 +7,17 @@ from kafka import KafkaConsumer
 def product_consumer(topic: str, bootstrap_servers: str):
     consumer = KafkaConsumer(topic, bootstrap_servers=bootstrap_servers)
     for message in consumer:
-        print(message.value)
+        print(f"sending message to user with id: {json.loads(message.value)['id']}")
+        print(f"message: {json.loads(message.value)}")
+        print("calculating the total price of the orders")
+        total_price = 0
+        total_price += json.loads(message.value)["product_price"]
+        for i in range(int(total_price)):
+            print("processing order")
+        print(f"total price: {total_price}")
+        print("sending the total price to the user")
+        print("-----------------------------------------------------")
+        print("-----------------------------------------------------")
 
 
 if __name__ == "__main__":
